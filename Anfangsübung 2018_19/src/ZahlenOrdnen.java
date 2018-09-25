62863d84dcb980595bd1118d1f256daccd607541
Roshy2 <roshy@live.at>
2018-09-25 14:50:56 +0200

    hallo7

--- Anfangsübung 2018_19/src/ZahlenOrdnen.java
+++ Anfangsübung 2018_19/src/ZahlenOrdnen.java
@@ -3,34 +3,21 @@ import java.util.ArrayList;
 public class ZahlenOrdnen {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> l = new  ArrayList<Integer>();
-		for(int ausgabe : l){
-		System.out.println(ausgabe);
-		}
-		}
-	
-	public static void Sort(ArrayList<Integer> zahl ){
-		ArrayList<Integer> zahl1 = new  ArrayList<Integer>();
-		ArrayList<Integer> l = new  ArrayList<Integer>();
-		zahl1.add(8);
-		zahl1.add(4);
-		zahl1.add(4);
-		zahl1.add(3);
-		zahl1.add(12);
-		zahl1.add(6);
-		zahl1.add(15);
-		zahl1.add(7);
-		
-		
-			for(int i=1;1<=100;i++){
-				
-					if(zahl1.size() % i == 0){
-						l.add(zahl1);
+		int pos = 0;
+		ArrayList<Integer> z = new ArrayList<Integer>();
+
+		for (int zl = 0; zl < 10; zl++) {
+			z.add((int) ((Math.random()) * 100 + 1));
+
+			for (int j = 0; j < zl; j++) {
+				for (int i = 0; i < z.size() - 1; i++) {
+					if (z.get(i) > z.get(i + 1)) {
+						z.set(i, z.get(i + 1));
+						z.set(i + 1, z.get(i));
 					}
-				
+				}
 			}
 		}
-		
-	
-
+		System.out.println(z);
+	}
 }
